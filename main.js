f92179853d63c91c370b3968507ba4463aa863f6
hoyamm <hoyazi329@gmail.com>
2023-01-10 16:58:43 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+/* p04_'스크롤 하여 더 보기' 버튼 */
+/* const scrollBtn = document.querySelectorAll(".scrollBtn");
+const part04 = document.querySelectorAll(".part04");
+
+const firstTop = part04[0].offsetTop;
+
+scrollBtn[0].onclick = function() {
+    window.scroll({
+        top:firstTop, behavior:"smooth"
+    });
+}; */
+
+//화살표를 클릭하면 텍스트박스가 나타납니다.
+
